0582cc064dc5bc9dc81b5f2fe079b67a71b0ac5a
NawelB <nawel.berqouch@gmail.com>
2020-07-02 22:59:32 +0200

    get all data of 1 event

--- src/app/update-event/update-event.component.ts
+++ src/app/update-event/update-event.component.ts
@@ -33,11 +33,9 @@ private getEvent(_id:string){
  return this._eventService.findById(_id).subscribe(
     data => {
       this.form=data;
-      let title1=this.form.title1;
-      let title2=this.form.title2;
-      let img1=this.form.img1;
-      
+     
       console.log("data "+this.form.title1)
+
       this.evenement = new Event(
       this.form._id,
       this.form.title1,
@@ -57,44 +55,9 @@ private getEvent(_id:string){
       this.form.submitAbstract,
       this.form.register, 
       ) 
-      /* this.evenement._id=this.form._id;
-      console.log(this.evenement._id)
-      this.form.title1 = data.title1;
-      this.form.title2=data.title2;
-      this.form.img1=data.img1;
-      this.form.img2=data.img2;
-      this.form.description=data.description;
-      this.form.date=data.date;
-      this.form.dateDebut=data.dateDebut;
-      this.form.dateFin=data.dateFin;
-      this.form.lieu=data.lieu;
-      this.form.city=data.city;
-      this.form.country=data.country;
-      this.form.email=data.email;
-      this.form.siteWeb=data.siteWeb;
-      this.form.tags=data.tags;
-      this.form.submitAbstract=data.submitAbstract;
-      this.form.register=data.register;
- */
-     /*  this.event._id=data._id;
-      this.event.title1 = data.title1;
-      this.event.title2=data.title2;
-      this.event.img1=data.img1;
-      this.event.img2=data.img2;
-      this.event.description=data.description;
-      this.event.date=data.date;
-      this.event.dateDebut=data.dateDebut;
-      this.event.dateFin=data.dateFin;
-      this.event.lieu=data.lieu;
-      this.event.city=data.city;
-      this.event.country=data.country;
-      this.event.email=data.email;
-      this.event.siteWeb=data.siteWeb;
-      this.event.tags=data.tags;
-      this.event.submitAbstract=data.submitAbstract;
-      this.event.register=data.register; */
-      //this.event=data;
+     
       console.log("***********data "+ this.evenement._id);
+      console.log("***********data "+ this.form.siteWeb);
       console.log("event "+ JSON.stringify(this.evenement))},
       (error) => { console.log(error)}
   )
