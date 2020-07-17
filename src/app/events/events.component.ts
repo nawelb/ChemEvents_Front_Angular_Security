7355fa0d834d81618930bb4768c54433f8dcf743
BERQOUCH <5107270-NawelB@users.noreply.gitlab.com>
2020-07-17 17:58:34 +0200

    filter by date

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -15,24 +15,65 @@ export class EventsComponent implements OnInit {
   city:string;
   country:string;
   listeEvents : Event[];
+  date:string;
+  today= new Date();
   //research: ResearchComponent;
   constructor(private _eventService : EventService,/*  public researchLinkService:ResearchLinkService */) { 
     //this.researchLinkService.listeEvents;
   }
 
   ngOnInit(): void{
+    this.formatDate(this.today)
+    this.date = JSON.stringify(this.formatDate(this.today))
     this.getAllEvents();
-    console.log(getLocaleDateTimeFormat)
+    console.log(this.date)
   };
 
    getAllEvents(): void{
     this._eventService.findAll().subscribe(
       data => {
         this.listeEvents=data,
-        console.log(data)
+        //this.date = (this.today.getFullYear()+'-'+(this.today.getMonth()+1)+'-'+this.today.getDate());
+        //this.formatDate(this.today)
+       /*  this.date = JSON.stringify(this.formatDate(this.today))
+        console.log("++++++++++"+this.date) */
+         this.listeEvents.sort(function (a:Event, b:Event) {
+        //   console.log("V222222"+this.date)
+          return a.dateDebut.localeCompare(b.dateDebut);
+         // a.dateDebut.localeCompare(b.dateDebut); 
+          //return this.listeEvents.
+      //  }).filter(event => event.dateDebut > this.date);; 
+      })
+
       }, (error) => {console.log(error)}
       ); 
   } 
+   
+
+  formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+/* 
+afterToday(event : Event){
+  //liste.forEach(element => {
+
+    event.dateDebut > this.date;
+    
+  //});
+}
+ */
+
+
 
   getByCity(city:string){
     this._eventService.findByCity(this.city).subscribe(
